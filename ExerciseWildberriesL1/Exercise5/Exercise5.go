07cf118321b12b91274a4dd4cdc5531afeb7a7fc
Денис <92530815+denchec@users.noreply.github.com>
2022-04-22 16:09:47 +0300

    Update Exercise5.go

--- ExerciseWildberriesL1/Exercise5/Exercise5.go
+++ ExerciseWildberriesL1/Exercise5/Exercise5.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -11,36 +10,28 @@ func main() {
 	_, err := fmt.Scan(&timeValue)
 	checkError(err)
 
-	wg := sync.WaitGroup{}
-
 	ch := make(chan int, 1)
 
 	exitTime := time.After(time.Second * timeValue)
 
 	var sequenceOfNumbers []int
 
-	wg.Add(1)
-	go func() {
-		num := 1
-		for {
-			select {
-			case <-exitTime:
-				wg.Done()
-				return
-				/* Если время еще НЕ истекло, то:
-				1) в канал передается число из переменной num
-				2) число забирается из канала и добавляется в срез sequenceOfNumbers
-				3) переменная num увеличивается на 1*/
-			default:
-				ch <- num
-				sequenceOfNumbers = append(sequenceOfNumbers, <-ch)
-				num += 1
-			}
+	num := 1
+	for {
+		select {
+		case <-exitTime:
+			fmt.Println(len(sequenceOfNumbers))
+			return
+			/* Если время еще НЕ истекло, то:
+			1) в канал передается число из переменной num
+			2) число забирается из канала и добавляется в срез sequenceOfNumbers
+			3) переменная num увеличивается на 1*/
+		default:
+			ch <- num
+			sequenceOfNumbers = append(sequenceOfNumbers, <-ch)
+			num += 1
 		}
-	}()
-
-	wg.Wait()
-	fmt.Println(len(sequenceOfNumbers)) // Выводим количество новых записей, созданных за отведенный промежуток времени
+	}
 }
 
 func checkError(err error) {
